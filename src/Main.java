96c818ddc8a75211214fbc309d106e752abb3b2c
Ksenia V <vekerle.ksenia@yandex.com>
2024-04-21 19:03:48 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int cost = 26897; // стоимость билета
+        int rub = 20; // количество рублей для одной бонусной мили
+
+        System.out.println((cost / rub) + "количество бонусных миль");
+
+
+    }
+
+}
